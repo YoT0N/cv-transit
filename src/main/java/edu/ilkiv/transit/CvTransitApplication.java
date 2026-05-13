201b281f5a878a0e06e0297e7e85c82309021618
YoT0N <ilkiv.bo@gmail.com>
2026-05-14 00:05:28 +0300

    Initial commit

---
+++ src/main/java/edu/ilkiv/transit/CvTransitApplication.java
@@ -0,0 +1,13 @@
+package edu.ilkiv.transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvTransitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvTransitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ilkiv/transit/CvTransitApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ilkiv.transit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvTransitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
